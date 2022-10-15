fa965db1c4a5a11ad9be60b61886f08644cbbb69
UmerKhan2K <105521080+UmerKhan2K@users.noreply.github.com>
2022-10-16 01:53:53 +0500

    Update firebase.js

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 const firebaseConfig = {
-    apiKey: "AIzaSyDlBzoHyU6aNi8sNZ8_ceb3MYlDUnlGkDw",
-    authDomain: "clone-35191.firebaseapp.com",
-    projectId: "clone-35191",
-    storageBucket: "clone-35191.appspot.com",
-    messagingSenderId: "388437558275",
-    appId: "1:388437558275:web:95947e425c1ba9e483fa36"
+    //firebase config
   };
 
   const firebaseApp = initializeApp(firebaseConfig);
